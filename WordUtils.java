83bf0a0e936d7950fb8defd5c38c8d67f9a267b4
MarchukO <omarchuk@me.bergen.edu>
2026-07-30 20:56:53 -0400

    Add method to check if two words differ by one letter

--- WordUtils.java
+++ WordUtils.java
@@ -1,1 +1,24 @@
+public class WordUtils {
+    public static boolean differsByOneLetter(
+            String firstWord, String secondWord) {
 
+        if (firstWord == null || secondWord == null) {
+            return false;
+        }
+        if (firstWord.length() != secondWord.length()) {
+            return false;
+        }
+
+        int differences = 0;
+        for (int index = 0; index < firstWord.length(); index++) {
+            if (firstWord.charAt(index) != secondWord.charAt(index)) {
+                differences++;
+
+                if (differences > 1) {
+                    return false;
+                }
+            }
+        }
+        return differences == 1;
+    }
+}
